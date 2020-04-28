6861dd66a288656ad0aa13d87b67d976f1f753cd
sanody <36520315+sanody@users.noreply.github.com>
2020-04-28 22:06:41 +0200

    Update test_read_from_file.py

--- test_read_from_file.py
+++ test_read_from_file.py
@@ -1,27 +1,27 @@
-def count(arry_x,mim):
+def count(arry_x,mim,suportA):
     count_arry=[]
-    refranc=[]
-    get_index=0
-    Support=0
-    for x in arry_x :
-        
-        if  (x in refranc)==0:
-            cou=arry_x.count(x)
+    refranc   =[]
+    get_index =0
+    Support   =0
+    for x in arry_x :       
+        if (x in refranc) == 0 :
+            cou = arry_x.count(x)
             if cou >= mim:
                 Support=cou/len(arry_x)
                 
                 refranc.append(x)
                 count_arry.append([[x], Support , [get_index] ] )
-                
+                suportA.append([[x], Support])
         else :
-            index1=refranc.index(x)
+            index1 = refranc.index(x)
             count_arry[index1][2].append(get_index)
         get_index = get_index+1;    
     return count_arry
 
-##########################################################################
 
-def SupportTwoItemSet( SupportA , SupportB , total , minSupport):
+############################################
+
+def SupportItemSet( SupportA , SupportB , total , minSupport):
   index = []
   supportArray = []
   count = 0
@@ -37,7 +37,7 @@ def SupportTwoItemSet( SupportA , SupportB , total , minSupport):
           support = count/total
 
           if(support >= minSupport ):
-              firsttuple = firstIndex[0] + secondIndex[0] 
+              firsttuple = firstIndex[0] + secondIndex[0]
               supportArray.append([ firsttuple , support , index ])
 
           index = []
@@ -48,10 +48,10 @@ def SupportTwoItemSet( SupportA , SupportB , total , minSupport):
 ###################################################3
 
 
-def get_first(original , min_count):
+def get_first(original , min_count , SupportAar):
   first_set = []
   for i in original :
-    first_set.append(count(i,min_count))
+    first_set.append(count(i,min_count, SupportAar))
   return first_set
 
 
@@ -74,40 +74,15 @@ def get_Item (first_set , Any_list , Total_count ,min_support):
 
   return set_A
 
-
 #####################################################
 #####################################################
 
-f = open("ticdata2000.txt", "r")
-A=[]
-B=[]
-C=[]
-D=[]
-E=[]
-F=[]
-G=[]
-H=[]
-I=[]
-J=[]
-K=[]
-L=[]
-
-for x in f:
-    #m=x[6:28]
-    data = x.split("\t")
-    A.append('A'+ data[5])
-    B.append('B'+ data[6])
-    C.append('C'+ data[7])
-    D.append('D'+ data[8])
-    E.append('E'+ data[9])
-    F.append('F'+ data[10])
-    G.append('G'+ data[11])
-    H.append('H'+ data[12])
-    I.append('I'+ data[13])
-    J.append('J'+ data[14])
-    K.append('K'+ data[15])
-    L.append('L'+ data[16])
-
+A=['A1','A3','A1','A3','A1','A3','A2']
+B=['B2','B4','B2','B4','B2','B5','B6']
+C=['C3','C5','C6','C5','C6','C1','C1']
+D=['D4','D3','D4','D3','D4','D5','D5']
+E=['E8','E1','E8','E1','E6','E6','E9']
+F=['F5','F8','F6','F5','F6','F8','F5']
 
 original = []
 
@@ -117,24 +92,45 @@ original.append(C)
 original.append(D)
 original.append(E)
 original.append(F)
-original.append(H)
-original.append(I)
-original.append(J)
-original.append(K)
-original.append(L)
 
+SupportAar = []
+lows       = []
 
 Total_count = len(A)
 min_count   = int(input("please input min count "))
 min_support = min_count / Total_count
 
-first_set  = get_first(original , min_count)
+first_set  = get_first(original , min_count , SupportAar)
 final_list = first_set
 
 while True: 
      temp =  get_Item (first_set , final_list , Total_count ,min_support)
      if len(temp) != 0:
         final_list = temp
+        for j in final_list:
+          for i in j:
+            SupportAar.append([i[0],i[1]])
      else:
        break
 
+for j in final_list:
+  for i in j:
+    for k in i[0]:
+      index = i[0].index(k)
+      X = [k]
+      Y = i[0][0:index]+i[0][index+1:]
+      for XY in SupportAar:
+        if X == XY[0]:
+          supportX = XY[1]
+        elif Y == XY[0]:
+          supportY = XY[1]
+
+      lows.append([X , Y ,i[1] , supportX , supportY])
+      lows.append([Y , X ,i[1] , supportX , supportY])
+
+for i in final_list:
+  for j in i:
+    print(j)
+
+for i in lows:   
+    print(i)
